d875c486c08bfcb07bf8b382e33be5965d5002f5
Dinesh <51910892+dineshindn@users.noreply.github.com>
2021-08-23 12:51:28 +0530

    Create application_start.sh

---
+++ scripts/application_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo chmod -R 777 /home/ubuntu/nodejs-express
+#navifate into our worling directory where we have all our github files
+cd /home/ubuntu/nodejs-express
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_copletion (node is in)
+#mysql create table
+# sudo mysql;
+# use sim_manager_development;
+
+# CREATE TABLE IF NOT EXISTS `test1` (
+#   `id` bigint(20) NOT NULL PRIMARY KEY,
+#   `name` varchar(30) NOT NULL
+# ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+# exit;
+#install node modules
+npm install 
+# pm2 start npm --name "myApp" -- run "start" 
+#start our node app in the background
+node app.js > app.out.log 2> app.err.log < /dev/null &
+
+
